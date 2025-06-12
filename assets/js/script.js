0569fc8bd481bb4889ebb1eb2a190a765944f74e
Roeich <roeichsheikh@gmail.com>
2025-06-12 18:00:25 +0600

    updated functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,19 +28,68 @@ $(document).ready(function() {
     $('[data-check]').on('click', function() {
         const id = $(this).data('check');
         $('#' + id).prop('checked', true);
+        orderPreviewItems();
     });
 
     // Handle uncheck
     $('[data-uncheck]').on('click', function() {
         const id = $(this).data('uncheck');
         $('#' + id).prop('checked', false);
+        orderPreviewItems();
     });
+
+    // 
+    $(".form_select__input").on("change", function() {
+        orderPreviewItems();
+    })
+
+    // order preview items
+    function orderPreviewItems() {
+        $(".order_preview__items .order_preview__item").remove();
+        let inner_html = ``;
+        $(".form_select").each((ind, elm) => {
+            if ($(elm).find(".form_select__input").is(":checked")) {
+                inner_html += `
+                    <tr class="order_preview__item">
+                        <td>1x</td>
+                        <td>${$(elm).find(".form_select__title").html()}</td>
+                        <td>
+                `;
+                if($(elm).find(".form_select__oldPrice").html()){
+                    inner_html+=`
+                            <span class="order_preivew__oldPrice">${$(elm).find(".form_select__oldPrice").html()}</span><br>
+                            <b class="order_preview__price">${$(elm).find(".form_select__price").html()}</b>
+                        </td>
+                    </tr>
+                    `;
+                }else{
+                    inner_html+=`
+                            <b class="order_preview__price">${$(elm).find(".form_select__price").html()}</b>
+                        </td>
+                    </tr>
+                    `;
+                }
+            }
+        });
+        $(".order_preview__items").append(inner_html);
+        calculateTotalPrice();
+    }
+    function calculateTotalPrice(){
+        let totalPrice=0;
+        let unitName=$(".order_preview__totalPrice").text().split(" ")[1];
+        $(".order_preview__items .order_preview__price").each((ind,elm)=>{
+            const price=parseFloat($(elm).text());
+            totalPrice+=price;
+        });
+        $(".order_preview__totalPrice").html(totalPrice+" "+unitName);
+    }
+    orderPreviewItems();
 
     // product modal slider
 
     $(".prdModal_slider").owlCarousel({
-        items:1,
-        dots:true,
-        nav:false
+        items: 1,
+        dots: true,
+        nav: false
     })
 });
